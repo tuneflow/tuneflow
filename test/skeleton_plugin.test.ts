000434cd6279eaba6560cd444042be07cb5014ea
Mingzhi Zeng <zeostudio@gmail.com>
2022-01-10 22:09:08 -0800

    fix tests

--- test/skeleton_plugin.test.ts
+++ test/skeleton_plugin.test.ts
@@ -1,7 +1,17 @@
+import type { SongAccess } from '../src';
 import { TuneflowPipeline, Song, TuneflowPlugin } from '../src';
 
 describe('Skeleton Tuneflow', () => {
-  class SkeletonPlugin extends TuneflowPlugin {
+  class TestUtilsPlugin extends TuneflowPlugin {
+    songAccess(): SongAccess {
+      return {
+        createTrack: true,
+        removeTrack: true,
+      };
+    }
+  }
+
+  class NoAccessCreateTrackPlugin extends TuneflowPlugin {
     static providerId(): string {
       return 'andantei';
     }
@@ -20,18 +30,35 @@ describe('Skeleton Tuneflow', () => {
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     async run(song: Song, inputs: any[]): Promise<{ [artifactId: string]: any } | void> {
-      song.createTrack();
+      song.createTrack({});
     }
   }
 
+  class CreateTrackPlugin extends NoAccessCreateTrackPlugin {
+    songAccess(): SongAccess {
+      return {
+        createTrack: true,
+        removeTrack: true,
+      };
+    }
+  }
+
+  const testUtilsPlugin = new TestUtilsPlugin();
+  let song = new Song();
+
+  beforeEach(() => {
+    song = new Song();
+    // @ts-ignore
+    song.setPluginContextInternal(testUtilsPlugin);
+  });
+
   it('finishes correctly', async () => {
-    const song = new Song();
     song.createTempoChange({
       ticks: 0,
       bpm: 120,
     });
     song.setResolution(480);
-    const track = song.createTrack();
+    const track = song.createTrack({});
     track.createNote({
       pitch: 64,
       velocity: 80,
@@ -39,9 +66,29 @@ describe('Skeleton Tuneflow', () => {
       endTick: 10,
     });
     const pipeline = new TuneflowPipeline();
-    pipeline.addPluginAt(new SkeletonPlugin(), 0);
+    pipeline.addPluginAt(new CreateTrackPlugin(), 0);
     expect(song.getTracks().length).toBe(1);
     await pipeline.run(song);
     expect(song.getTracks().length).toBe(2);
+  });
+
+  describe('Song Access', () => {
+    beforeEach(() => {
+      song = new Song();
+    });
+
+    it('throws error if calling restricted methods on song without setting plugin context.', async () => {
+      expect(() => {
+        song.createTrack({});
+      }).toThrow();
+    });
+
+    it('throws error if plugin calls restricted methods without getting access.', async () => {
+      const pipeline = new TuneflowPipeline();
+      pipeline.addPluginAt(new NoAccessCreateTrackPlugin(), 0);
+      expect(song.getTracks().length).toBe(0);
+      expect(pipeline.run(song)).rejects.toBeTruthy();
+      expect(song.getTracks().length).toBe(0);
+    });
   });
 });
