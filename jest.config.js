75044b922399f1731bf47250780629b3cb046e18
Mingzhi Zeng <zeostudio@gmail.com>
2022-04-04 20:06:37 -0700

    add junit reporter

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
+  reporters: ['default', 'jest-junit'],
   coverageReporters: ['cobertura', 'text-summary'],
 };
