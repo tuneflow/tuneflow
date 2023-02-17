9a187e5be2768fd5b506cac873c55fac36742c5c
Andantei行板 <97192387+andantei@users.noreply.github.com>
2023-02-16 23:10:14 -0800

    collect jest coverage information

--- jest.config.js
+++ jest.config.js
@@ -3,6 +3,18 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   collectCoverage: true,
+  transform: {
+    '^.+\\.(j|t)s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['js', 'ts'],
+  coveragePathIgnorePatterns: [
+    '/node_modules/',
+    '<rootDir>/build',
+    '<rootDir>/dist',
+    '<rootDir>/docs',
+    '<rootDir>/test',
+  ],
+  transformIgnorePatterns: ['<rootDir>/node_modules/'],
   reporters: ['default', 'jest-junit'],
-  coverageReporters: ['cobertura', 'text-summary'],
+  coverageReporters: ['cobertura', 'text-summary', 'clover'],
 };
